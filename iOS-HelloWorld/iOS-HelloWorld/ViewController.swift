21b113498abb32789971141e5d30d8b7fb8b097f
russellry <russell.ong@tbcasoft.com>
2020-07-21 15:44:01 +0800

    Add iOS Project

---
+++ iOS-HelloWorld/iOS-HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-HelloWorld
+//
+//  Created by Russell Ong on 7/21/20.
+//  Copyright © 2020 Russell Ong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
